2f7f4f569c321a3ff20400d459adef57ea33d343
Sonal Girhe <sonalgirhe1920@gmail.com>
2022-12-09 22:18:02 +0530

    Dialog

---
+++ lib/Simple.dart
@@ -0,0 +1,23 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SimpleScreen extends StatelessWidget{
+@override
+Widget build(BuildContext context){
+  return Scaffold(body: Center(
+    child: Center(
+      child: Container(
+        width: 200,
+        height: 200,
+        color: Colors.yellow,
+        child: Center(child: Text ("I Am SimpleScreen"))
+  
+  
+      ),
+    ),
+  )
+  );
+}
+}
---
+++ lib/TimerPicker.dart
@@ -0,0 +1,23 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class TimePickerScreen extends StatelessWidget{
+@override
+Widget build(BuildContext context){
+  return Scaffold(body: Center(
+    child: Center(
+      child: Container(
+        width: 200,
+        height: 200,
+        color: Colors.blueGrey,
+        child: Center(child: Text ("I Am TimePickerScreen"))
+  
+  
+      ),
+    ),
+  )
+  );
+}
+}
